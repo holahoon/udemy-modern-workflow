c483c5c9c02748f35647813f13afcc6b1d66988a
David Kim <david.kim@luckie.com>
2020-04-08 15:20:58 -0400

    Completed Header section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,13 @@
-// Important css files
+// Import css files
 import "../styles/styles.css";
+
+// Import JS files
+import MobileMenu from "./modules/MobileMenu";
 
 // Accept the hot update if it makes sense (updates on the fly)
 if (module.hot) {
   module.hot.accept();
 }
+
+/* === This is where the functionality starts === */
+const mobileMenu = new MobileMenu();
