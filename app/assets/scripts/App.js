f72b38d6baccbdb5222f33816a6f68cde2e6ad33
David Kim <david.kim@luckie.com>
2020-04-09 21:40:39 -0400

    Sticky headers completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,6 +4,7 @@ import "../styles/styles.css";
 // Import JS files
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnSCroll from "./modules/RevealOnScroll";
+import StickyHeader from "./modules/StickyHeader";
 
 // Accept the hot update if it makes sense (updates on the fly)
 if (module.hot) {
@@ -11,6 +12,13 @@ if (module.hot) {
 }
 
 /* === This is where the functionality starts === */
+
+// Mobile menu
 const mobileMenu = new MobileMenu();
+
+// Reveal contents on scroll
 new RevealOnSCroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnSCroll(document.querySelectorAll(".testimonial"), 60);
+
+// Sticky header on scroll on desktop
+const stickyHeader = new StickyHeader();
