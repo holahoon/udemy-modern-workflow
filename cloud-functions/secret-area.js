7a07c269b3abda66ccc757b5442e8d8be6976124
David Kim <david.kim@luckie.com>
2020-04-14 11:55:12 -0400

    added local CORS solution

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,4 +1,18 @@
 exports.handler = (event, context, callback) => {
+  /* To test the cloud function locally - CORS solution */
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type"
+  };
+
+  if (event.httpMethod !== "POST") {
+    return callback(null, {
+      statusCode: 200,
+      headers,
+      body: "This was not a POST request"
+    });
+  }
+
   const secretContent = `
         <h3>Welcome to the secret area!</h3>
         <p>Here, we can tell you that your <i>password</i> was correct!</p>
@@ -15,11 +29,13 @@ exports.handler = (event, context, callback) => {
   if (body.password == "javascript") {
     callback(null, {
       statusCode: 200,
+      headers,
       body: secretContent
     });
   } else {
     callback(null, {
-      statusCode: 401
+      statusCode: 401,
+      headers
     });
   }
 };
